ab37661e07d861bfbd699c261f5eb477ead58d1a
Skyz03 <aakibdiscoo@gmail.com>
2022-03-17 19:52:55 +0545

    Updates in the image section of the code

--- src/data.js
+++ src/data.js
@@ -19,4 +19,14 @@ export default [
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
     imageUrl: "https://source.unsplash.com/JmuyB_LibRo",
   },
+  {
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "01 Oct, 2021 ",
+    endDate: "18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+  },
 ];
