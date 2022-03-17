43ce098becf0ecb1e1b776cb0c064331b2d29f12
Skyz03 <aakibdiscoo@gmail.com>
2022-03-17 17:43:43 +0545

    Updates in the card content styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./components/Card";
 import data from "./data";
+import "./index.scss";
 
 const App = () => {
   // THIS IS FOR CONSTANT MAP
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
 import React from "react";
+import map from "../map.png";
 
 const Card = (props) => {
   return (
     <div className="card">
       <div className="card__img">
-        <img src={props.item.imageUrl} alt="" />
+        <img src={props.item.imageUrl} alt="country-scene" />
       </div>
 
       <div className="card__content">
         <div className="card__location">
-          {/* img icon for location goes here  */}
-          <span>{props.item.location} </span>
-          <a>View on Google Maps</a>
+          <span>
+            <img src={map} alt="map-icon" />
+          </span>
+          <span className="card__country">{props.item.location} </span>
+          <a className="card__link" href="google.com">
+            View on Google Maps
+          </a>
         </div>
 
         <h1>{props.item.title}</h1>
