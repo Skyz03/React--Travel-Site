82592ecbf6b2baee2e2003cc9afa9f7faafa6d28
Skyz03 <aakibdiscoo@gmail.com>
2022-03-17 16:17:27 +0545

    Updates in the content of the site using props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Card from "./components/Card";
 import data from "./data";
 
 const App = () => {
+  // THIS IS FOR CONSTANT MAP
   const cards = data.map((item) => {
-    return item.title;
+    return <Card key="" item={item} />;
   });
 
-  console.log(cards);
-  return <Card key="" />;
+  // console.log(cards);
+
+  return <div>{cards}</div>;
 };
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,26 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <div className="card__img">
-        <img src="https://source.unsplash.com/WLxQvbMyfas" />
+        <img src={props.item.imageUrl} alt="" />
       </div>
 
-      <div className="card__content"></div>
+      <div className="card__content">
+        <div className="card__location">
+          {/* img icon for location goes here  */}
+          <span>{props.item.location} </span>
+          <a>View on Google Maps</a>
+        </div>
+
+        <h1>{props.item.title}</h1>
+        <p>
+          {props.item.startDate} - {props.item.endDate}
+        </p>
+
+        <p>{props.item.description}</p>
+      </div>
     </div>
   );
 };
